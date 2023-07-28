416ddc66a41c0cac22bc5dd00328e707263ec147
mscotti <mscotti@pigmalion.com.ar>
2023-07-28 17:26:00 -0300

    Se modificó la configuración para el dominio marianoscotti.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), mdx(), image()],
-	site: 'https://scottimariano.github.io',
-	base: '/portfolio',
+	site: 'https://marianoscotti.com',
 });
