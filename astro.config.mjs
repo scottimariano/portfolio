cfd5e3b4beab2a702d0e49f82bad0fad5d6df893
scottimariano <scottimariano@gmail.com>
2024-09-03 08:55:41 -0300

    Se actualizó la version de node necesaria para el deploy v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), mdx(), image()],
-	site: 'https://marianoscotti.com',
+	site: 'https://marianoscotti.com.ar',
 });
